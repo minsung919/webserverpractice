0b1866a479bed5e7fc5597b093cc02e8a78361b9
minsung <minsung0919@gmail.com>
2024-06-11 12:45:16 +0900

    24-06-11

--- src/db_manager/index.js
+++ src/db_manager/index.js
@@ -1,15 +1,41 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+const { Model, DataTypes ,Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('webservice', 'root', '1234', {
     host: 'localhost',
     dialect:'mysql',
     
   });
+  
+
+class User extends Model {}
+
+User.init({
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    pwd: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User' // 테이블 이름 지정
+});
+
+
 
 const connectTestDb = async () => {
   try {
     await sequelize.authenticate();
     console.log('DB연결 성공');
+    await User.sync();
   } catch (error) {
     console.error('DB연결 실패', error);
   }
